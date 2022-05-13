ae1758a3ec8ff8ea4ed7019bd62bb9c89307878b
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-12 21:37:29 -0300

    introdução à Java Script e lógica de Programação

---
+++ fundamentos/bloco-4/dia_4.1/primeiros-passos.js
@@ -0,0 +1,14 @@
+const myName = "Henrique";
+const bithCity = "São Bernardo do Campo";
+var bithYear = 1999;
+
+var bithYear = 2030;
+const bithCity = Diadema
+
+console.log(myName);
+console.log(bithCity);
+console.log(bithYear);
+
+//Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+
+//R: Deu erro, por que não é possivel alterar o valor de uma (CONST).
