b2aac0f1251a9dee437abb3d4160f1b49840f8df
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-13 01:10:20 -0300

    operadores Logicos

---
+++ fundamentos/bloco-4/dia_4.1/operadores_logicos/operador-NOT.js
@@ -0,0 +1,5 @@
+console.log((2 + 2) === 4);
+//Ira retornar um valor verdadeiro
+
+console.log(!(2 + 2) === 4);
+//Ira retornar um valor false. O operador ( ! ) sempre fara a opcão oposta do que é true.
---
+++ fundamentos/bloco-4/dia_4.1/operadores_logicos/operador-OR.js
@@ -0,0 +1,9 @@
+var weekDay = "quarta-feira"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else if (weekDay === "sabado" || weekDay === "domingo") {
+    console.log("FINALMENTE, descanso merecido UwU");
+} else {
+    console.log("Desculpe não conheço esse dia");
+}
