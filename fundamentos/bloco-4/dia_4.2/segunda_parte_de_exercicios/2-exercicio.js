ef7973f443bcfc0982cf558edf5c3425e1e77a82
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-13 20:27:16 -0300

    exercicio 2 concluido

--- fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/2-exercicio.js
+++ fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/2-exercicio.js
@@ -1,3 +1,8 @@
 //Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(index = 0; index <= numbers.length -1 ; index += 1) {
+    //let resultado = numbers = index;
+    console.log(numbers.length + [index]);
+}
