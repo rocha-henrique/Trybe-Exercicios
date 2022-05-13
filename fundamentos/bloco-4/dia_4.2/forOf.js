23b55240576f4d4299d472d108a33558b6a48cd1
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-13 18:07:17 -0300

    exercicio Fot/Of

---
+++ fundamentos/bloco-4/dia_4.2/forOf.js
@@ -0,0 +1,9 @@
+
+//Exercício 1
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let print of names) {
+    console.log(print)
+}
