97a494268d6920db78cdd207c640e53d1121d406
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-12 22:41:17 -0300

    tipos primitivos

---
+++ fundamentos/bloco-4/dia_4.1/tipos-primitivos-tipagem-dinamica.js
@@ -0,0 +1,18 @@
+let patientId = "50"; //Será interpretado como string por que está em aspas ""
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientAge); //Essa variavel não foi criada !!
+
+const base = 5;
+var height = 8;
+const area = base + height;
+const perimeter = area + area;
+
+console.log(area);
+console.log(perimeter);
