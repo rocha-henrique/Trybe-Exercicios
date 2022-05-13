840bca7fb2c63008450ca95794649c2488e67931
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-13 17:05:07 -0300

    Ecercicio Loop-For

---
+++ fundamentos/bloco-4/dia_4.2/loopFor.js
@@ -0,0 +1,13 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let contador = 0; contador < cars.length; contador += 1) {
+    console.log(cars[contador])
+}
+//Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log():
+
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+for(let index = 0; index < groceryList.length; index += 1 ) {
+    console.log(groceryList[index])
+}
