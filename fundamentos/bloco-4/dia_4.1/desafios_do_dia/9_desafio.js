eb8895ed92f067d783b6d7ea3a3212e4a3985b01
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-14 00:46:00 -0300

    Exercicio 9 concluido

---
+++ fundamentos/bloco-4/dia_4.1/desafios_do_dia/9_desafio.js
@@ -0,0 +1,11 @@
+//Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+
+const numberOne = (73653);
+const numberTwo = (125554489856);
+const numberThree = (461345428);
+
+if(numberOne %2 === 1 || numberTwo %2 === 1 || numberThree %2 === 1) {
+    console.log(true)
+} else {
+    console.log(false)
+}
