94286ac05e69715d3971eb308abeedee921c777e
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-13 21:20:50 -0300

    edited

--- fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/2-exercicio.js
+++ fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/2-exercicio.js
@@ -2,7 +2,3 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(index = 0; index <= numbers.length -1 ; index += 1) {
-    //let resultado = numbers = index;
-    console.log(numbers.length + [index]);
-}
--- fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/3-exercicio.js
+++ fundamentos/bloco-4/dia_4.2/segunda_parte_de_exercicios/3-exercicio.js
@@ -2,3 +2,7 @@
 //A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let contador = 0; contador < numbers.length; contador += 1 ) {
+    console.log(contador[contador])
+}
