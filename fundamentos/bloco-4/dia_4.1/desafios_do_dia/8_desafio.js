b034d1dc0a82c3ea04257369df5c9fe98515f4a9
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-14 00:40:51 -0300

    Exercicio 8 concluido

---
+++ fundamentos/bloco-4/dia_4.1/desafios_do_dia/8_desafio.js
@@ -0,0 +1,12 @@
+//🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+
+
+const numberOne = (1);
+const numberTwo = (28);
+const numberThree = (27);
+
+if(numberOne % 2 == 0 || numberTwo % 2 == 0 || numberThree % 2 == 0 ) {
+    console.log(true)
+} else {
+    console.log(false)
+}
