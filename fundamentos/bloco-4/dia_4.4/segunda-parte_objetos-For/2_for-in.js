f5dc589852a27da108d5e5ca6a12f24d4d60ee55
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-18 13:46:40 -0300

    Exercercicio de OBJETO concluido

--- fundamentos/bloco-4/dia_4.4/segunda-parte_objetos-For/2_for-in.js
+++ fundamentos/bloco-4/dia_4.4/segunda-parte_objetos-For/2_for-in.js
@@ -4,15 +4,17 @@ let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
     idade: 21,
-    livrosFavoritos: {
+    livrosFavoritos: [
+      {
         titulo: 'O Pior Dia de Todos',
         autor: 'Daniela Kopsch',
         editora: 'Tordesilhas',
       },
+    ],
   };
 
 
-console.log('O livro favorito de' + ' ' + leitor.nome + ' ' + leitor.sobrenome + ' ' + 'se chama' + ' ' + leitor.livrosFavoritos.titulo);
+console.log('O livro favorito de' + ' ' + leitor.nome + ' ' + leitor.sobrenome + ' ' + 'se chama' + ' ' + leitor.livrosFavoritos[0].titulo);
 
 //Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
 
