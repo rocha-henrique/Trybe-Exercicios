ceb6f3fa1b681c75ed06fec3ec38e3b1d807afa9
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-19 19:28:15 -0300

    segunda parte concluida

---
+++ fundamentos/bloco-5/dia_5.1/script.js
@@ -0,0 +1,39 @@
+function changetext(){
+    let paragraph = document.getElementsByTagName('p')[1];
+    paragraph.innerText = 'Estar morando fora do país'
+}
+changetext();
+
+function changecolor() {
+    let color = document.getElementsByClassName('main-content')
+    color.innerHTML = backgroundColor = rgb76,164,109;
+}
+changecolor();
+
+function changeSquareToWhite() {
+    let squareRed = document.getElementsByClassName(center-content)[0];
+    squareRed.style.background = 'white';
+}
+changeSquareToWhite;
+
+
+function exercicioQuatro() {
+    let titulo = document.getElementsByClassName('title')[0]; 
+    titulo.innerText = 'Exercícios 5.1 - JavaScript'; 
+}
+exercicioQuatro();
+
+function exercicioCinco() {
+    let paragraph = document.getElementById('p')[1]; 
+    paragraph.innerText = paragraph.innerHTML.toUpperCase(); 
+}
+exercicioCinco();
+
+ 
+function exercicioSeis() {
+    let paragraphs = document.getElementsByTagName('p');
+    for (i = 0 i < paragraphs.length; i += 1) {
+      console.log(paragraphs[i].innerText);
+    }
+}
+exercicioSeis();
