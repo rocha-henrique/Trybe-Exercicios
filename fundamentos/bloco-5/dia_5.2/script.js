ce91adcc6199509972fcec3848ac0e8ea7f275b0
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-20 19:04:31 -0300

    creio eu que exercicio dois esta certo

--- fundamentos/bloco-5/dia_5.2/script.js
+++ fundamentos/bloco-5/dia_5.2/script.js
@@ -4,3 +4,10 @@ let newTag = document.createElement('h1');
 newTag.innerText = 'Exercício 5.2';
 
 father.appendChild(newTag);
+
+
+let newMain = document.createElement('main');
+newMain.createAttribute = 'main-content';
+
+
+father.appendChild(newMain);
