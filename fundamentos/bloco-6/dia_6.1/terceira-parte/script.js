bd0a7f46d438708a23c1e8ab26dd31e0dd6df330
Henrique Rocha <rochahenrique.1023@gmail.com>
2022-05-31 23:02:41 -0300

    first commit EM

---
+++ fundamentos/bloco-6/dia_6.1/terceira-parte/script.js
@@ -0,0 +1,13 @@
+// 3 - Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga Carol Silva nos contou um pouco sobre como fazer isso, lembra?
+// 4 -Crie um botão que limpe as informações contidas nos campos;
+
+
+
+
+
+
+
+
+// 1 - A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+
+// 2 - Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela.
